8eb10716b2f4b81d5c0ed64b6ebabd529391177f
omyedravkar <omyedravkar7@gmail.com>
2026-07-16 14:33:47 +0530

    Added register controller and bug fixed

--- src/utils/cloudinary.js
+++ src/utils/cloudinary.js
@@ -2,31 +2,34 @@ import { v2 as cloudinary } from 'cloudinary'
 import fs from "fs"
 
 
-cloudinary.config({ 
-  cloud_name: process.env.ClOUDINARY_CLOUD_NAME, 
-  api_key: process.env.ClOUDINARY_API_KEY , 
+cloudinary.config({
+  cloud_name: process.env.ClOUDINARY_CLOUD_NAME,
+  api_key: process.env.ClOUDINARY_API_KEY,
   api_secret: process.env.ClOUDINARY_API_SECRET
 });
 
 const upoloadCloudinary = async (loadFilePath) => {
-    
-    try {
-        if(!localFilePath) return null 
-        //upload the file on cloudinary
-    const response = await cloudinary.uploader.upload(localFilePath , {
-        resource_type: "auto"
-      })
-      console.log("file is uploaded on cloudinary", 
+
+  try {
+    if (!localFilePath) return null
+    //upload the file on cloudinary
+    const response = await cloudinary.uploader.upload(localFilePath, {
+      resource_type: "auto"
+    })
+    console.log("file is uploaded on cloudinary",
       response.url);
-      return resonse ; 
-    } catch (error) {
-        
-      fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed 
-
-    }
-
+    return response;
+  } catch (error) {
+    fs.unlinkSync(localFilePath) // remove the locally saved temporary file as the upload operation got failed 
+    return null
+  }
 }
 
+
+export {upoloadOnCloudinary}
+
+
+/*
 cloudinary.v2.uploader
 .upload("dog.mp4", {
   resource_type: "video", 
@@ -34,3 +37,4 @@ cloudinary.v2.uploader
   overwrite: true, 
   notification_url: "https://mysite.example.com/notify_endpoint"})
 .then(result=>console.log(result));
+*/
